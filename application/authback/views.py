1671b5e1db4ba27e616fe5a884b005bd11774e20
l.marder <asertolpas@gmail.com>
2019-11-23 22:48:40 +0300

    upd

--- application/authback/views.py
+++ application/authback/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User, Bicycle
 
 def index(request):
     return HttpResponse('heh boi l l l l l l lallalalala')
+
+"""
+def createUser(request):
+    data = User(
+    name = ,
+    surname = ,
+    phone = ,
+    login = ,
+    passw = ,
+    email = ,
+    data = ,
+    )
+    data.save()
+"""
