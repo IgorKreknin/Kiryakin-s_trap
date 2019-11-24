22831a35fa9eff9d0e5be412fe9019a8f434f303
DarthPointer <48957477+DarthPointer@users.noreply.github.com>
2019-11-24 11:01:38 +0300

    Primitive security rating

--- application/API/arithmetics/Rating.py
+++ application/API/arithmetics/Rating.py
@@ -1,3 +1,7 @@
+cameras_impact = 50.0
+stolen_impact = 0.5
+returned_impact = 0.8
+
 class Rating:
     current_rating = 0
     number_of_rates = 0
@@ -13,3 +17,10 @@ class Rating:
         temp += new_rating
         number_of_rates += 1
         current_rating = temp/number_of_rates
+
+
+def sequrityRating(cameras, stolen_rate, returned_rate):            # (100 - CI/(1+cameras))*SI^(stolen - RI*returned)
+    base = 100 - cameras_impact/(1+cameras)
+    returned_part = returned_rate/stolen_rate
+    exp = stolen_rate*(1 - returned_impact*returned_part)
+    return base * (stolen_impact**exp)
