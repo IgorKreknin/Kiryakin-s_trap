5255014348671825cf34e916d292a07c91c1b49f
l.marder <asertolpas@gmail.com>
2019-11-24 16:13:15 +0300

    Final commit

--- application/static/app/src/components/WindowAbout.jsx
+++ application/static/app/src/components/WindowAbout.jsx
@@ -2,10 +2,27 @@ import React from 'react'
 import styles from '../styles/window_about.module.css'
 
 export class WindowAbout extends React.Component {
+	constructor() {
+		super();
+		this.state ={
+			value: "",
+		}
+
+		this._onClick = this._onClick.bind(this);
+	}
+
+	_onClick() {
+		document.dispatchEvent(new Event('printAll'));
+		console.log('qweqwe');
+	}
+
 	render() {
 		return (
+			<div>
 			<div className={ styles.container }>
 				<img src={ '' } alt="Photo" className={styles.photo} />
+			</div>
+			<div className={ styles.showAll } onClick={ this._onClick }>Back</div>
 			</div>
 		)
 	}
